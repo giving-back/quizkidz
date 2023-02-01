e91921fc5ca3821d225d1a9f694fe98d73ab058c
Christian Blake <christianblake@Macbook-Pro-3.local>
2023-02-01 00:49:33 +0000

    QUIZKIDZ-2 new user screen

--- lib/screen/new_user_screen.dart
+++ lib/screen/new_user_screen.dart
@@ -1,34 +1,124 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:quizkidz/components/custom_snack_alert.dart';
-import 'package:quizkidz/providers/auth_provider.dart';
+import 'package:quizkidz/util/util.dart';
 
 class NewUserScreen extends ConsumerWidget {
   const NewUserScreen({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final authService = ref.watch(authServicesProvider);
+    final userNameController = TextEditingController();
+    final formKey = GlobalKey<FormState>();
 
-    return Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: Column(
-        children: [
-          const Text('new user'),
-          IconButton(
-            onPressed: () async => await authService.signOut().then(
-                  (value) => value.match(
-                    (error) => ScaffoldMessenger.of(context)
-                      ..hideCurrentSnackBar()
-                      ..showSnackBar(
-                        CustomSnackAlert.showErrorSnackBar(),
-                      ),
-                    (r) {},
+    return Form(
+      key: formKey,
+      child: Container(
+        color: kBlueColor,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Expanded(
+              flex: 23,
+              child: Container(),
+            ),
+            Expanded(
+              flex: 54,
+              child: Padding(
+                padding: const EdgeInsets.all(15.0),
+                child: Container(
+                  decoration: const BoxDecoration(
+                    color: kWhiteColor,
+                    borderRadius: BorderRadius.only(
+                      topRight: Radius.circular(40.0),
+                      bottomRight: Radius.circular(40.0),
+                      topLeft: Radius.circular(40.0),
+                      bottomLeft: Radius.circular(40.0),
+                    ),
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.only(left: 20.0, right: 20.0),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        const Text(
+                          'Choose your Avatar & Username',
+                          style: TextStyle(
+                            color: Colors.black54,
+                            fontSize: 15,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        Column(
+                          children: [
+                            SizedBox(
+                              height: 80,
+                              child: ListView.builder(
+                                shrinkWrap: true,
+                                scrollDirection: Axis.horizontal,
+                                itemCount: kAvatarImages.length,
+                                itemBuilder: (context, index) {
+                                  return Padding(
+                                    padding: const EdgeInsets.all(2.0),
+                                    child: CircleAvatar(
+                                      radius: 35,
+                                      backgroundColor:
+                                          kAvatarImages[index].color,
+                                      backgroundImage: AssetImage(
+                                          kAvatarImages[index].image),
+                                    ),
+                                  );
+                                },
+                              ),
+                            ),
+                            const Padding(padding: EdgeInsets.only(top: 10.0)),
+                            TextFormField(
+                              validator: (value) {
+                                if (value == null || value.isEmpty) {
+                                  return "Let players see who you are.";
+                                }
+                                return null;
+                              },
+                              controller: userNameController,
+                              decoration: const InputDecoration(
+                                border: UnderlineInputBorder(),
+                                labelText: 'username',
+                                prefixIcon: Icon(Icons.verified_user_outlined),
+                              ),
+                            ),
+                          ],
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          children: [
+                            TextButton(
+                              child: const Text(
+                                'Lets Go!',
+                                style: TextStyle(
+                                    color: kBlueColor,
+                                    fontSize: 18,
+                                    fontWeight: FontWeight.bold),
+                              ),
+                              onPressed: () {
+                                if (formKey.currentState!.validate()) {
+                                  print(userNameController.text);
+                                }
+                              },
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
                   ),
                 ),
-            icon: const Icon(Icons.logout),
-          ),
-        ],
+              ),
+            ),
+            Expanded(
+              flex: 23,
+              child: Container(),
+            ),
+          ],
+        ),
       ),
     );
   }
