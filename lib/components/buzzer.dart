f7836cc45cf846040136b47a79c3b4842008b459
Christian Blake <christianblake@Macbook-Pro-3.local>
2023-02-23 10:50:14 +0000

    QUIZKIDZ-34 buzzer buzz action

--- lib/components/buzzer.dart
+++ lib/components/buzzer.dart
@@ -1,6 +1,10 @@
 // Flutter imports:
 import 'package:flutter/material.dart';
+
+// Package imports:
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+// Project imports:
 import 'package:quizkidz/providers/quiz_provider.dart';
 import 'package:quizkidz/util/util.dart';
 
--- lib/screen/player_screen.dart
+++ lib/screen/player_screen.dart
@@ -1,6 +1,10 @@
 // Flutter imports:
 import 'package:flutter/material.dart';
+
+// Package imports:
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+// Project imports:
 import 'package:quizkidz/components/buzzer.dart';
 import 'package:quizkidz/components/common_app_bar.dart';
 import 'package:quizkidz/components/quiz_leaderboard.dart';
